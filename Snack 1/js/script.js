440b5ce762383739a2858469bbd4eb3b5e45d4bf
nespolisimmaco <snespoli@yahoo.com>
2023-03-24 16:39:55 +0100

    Add: array of objects created

---
+++ Snack 1/js/script.js
@@ -0,0 +1,26 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+const bici = [
+    {
+        nome: "Pinarello",
+        peso: 7
+    },
+    {
+        nome: "Van Rysel",
+        peso: 10
+    },
+    {
+        nome: "Pinarello",
+        peso: 8
+    },
+    {
+        nome: "Van Rysel",
+        peso: 6
+    },
+    {
+        nome: "Triban",
+        peso: 9
+    },
+];
