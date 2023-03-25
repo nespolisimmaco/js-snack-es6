c93743dd8fc73750d60181a633ddd319c3a71347
nespolisimmaco <snespoli@yahoo.com>
2023-03-25 15:07:14 +0100

    Add: array of objects created

---
+++ Snack 2/js/script.js
@@ -0,0 +1,38 @@
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà:
+// Punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadre = [
+    {
+        nome: "Napoli",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Lazio",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Inter",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "MIlan",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Roma",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Atalanta",
+        punti: 0,
+        falliSubiti: 0,
+    },
+];
